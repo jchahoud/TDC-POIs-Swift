ab2969b58c8a2afc583772f9763386a834153abf
Juliana Chahoud <jchahoud@gmail.com>
2014-07-27 16:00:23 -0300

    Initial Commit

---
+++ TDC-POIsTests/TDC_POIsTests.swift
@@ -0,0 +1,36 @@
+//
+//  TDC_POIsTests.swift
+//  TDC-POIsTests
+//
+//  Created by Juliana Chahoud on 7/27/14.
+//  Copyright (c) 2014 jchahoud. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TDC_POIsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
